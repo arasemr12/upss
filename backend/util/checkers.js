4009012c883931941f97a068479f5e7618780b93
Aras emr <arasemr1234@gmail.com>
2022-09-02 00:05:41 +0300

    checker fix.

--- backend/util/checkers.js
+++ backend/util/checkers.js
@@ -2,25 +2,29 @@ const jsonwebtoken = require('jsonwebtoken');
 const user = require('../models/user');
 
 const checkuser = async (req,res,next) => {
-    if(!req.headers || !req.headers.authorization) return next();
+    try {
+        if(!req.headers || !req.headers.authorization) return next();
 
-    let token = req.headers.authorization.split(" ")[1];
-    if(!token) return next();
-
-    let cacheuser = jsonwebtoken.verify(token,process.env.SECRET);
-    if(!cacheuser) return next();
-
-    let myuser = await user.findOne({
-        _id:cacheuser.user._id
-    });
-
-    if(!myuser) return next();
-
-    myuser.password = null;
-
-    req.user = myuser;
-
-    next();
+        let token = req.headers.authorization.split(" ")[1];
+        if(!token) return next();
+    
+        let cacheuser = jsonwebtoken.verify(token,process.env.SECRET);
+        if(!cacheuser) return next();
+    
+        let myuser = await user.findOne({
+            _id:cacheuser.user._id
+        });
+    
+        if(!myuser) return next();
+    
+        delete myuser.password;
+    
+        req.user = myuser;
+    
+        return next();
+    } catch (error) {
+        return next();
+    }
 }
 
 module.exports = {checkuser};
